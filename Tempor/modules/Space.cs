241ebbb14ba664c684d0367be6df7421b6a5ea60
Jonas <thenixchecker@gmail.com>
2017-07-29 12:26:07 +0200

    Rewrote space potd command to use the NASA public API. This shortenend and cleaned up the code and formatting of the explanation but it requires an additional API key.

--- Tempor/modules/Space.cs
+++ Tempor/modules/Space.cs
@@ -1,63 +1,46 @@
-﻿using System;
-using System.Text;
+﻿using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
-using HtmlAgilityPack;
+using Newtonsoft.Json.Linq;
 
 namespace Tempor.modules
 {
     [Group("space")]
     public class Space : ModuleBase
     {
+        
         // °space pictureoftheday -> *image url*
         [Command("pictureoftheday"), Summary("Posts the NASA Astronomy picture of the day.")]
         [Alias("apod", "potd", "astronomypictureoftheday")]
         public async Task Potd()
         {
-            const string url = "https://apod.nasa.gov/apod/astropix.html";
-            var web = new HtmlWeb();
-            var doc = web.Load(url);
-
-            var relImageUrl = doc.DocumentNode.SelectSingleNode("//img[@src]").Attributes["src"].Value;
-            var root = doc.DocumentNode.SelectSingleNode("//b[.=' Explanation: ']").ParentNode;
-            var ex = new StringBuilder();
-			
-            foreach (var node in root.ChildNodes)
+            var apiKey = File.ReadAllText("../../api_key.txt");
+            var url = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "text/json";
+            JObject response;
+            
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
-                if (!node.HasChildNodes)
-                {
-                    var text = node.InnerText;
-                    if (!string.IsNullOrEmpty(text))
-                        ex.AppendLine(text.Trim());
-                } else {
-                    foreach (var innerNode in node.ChildNodes)
-                    {
-                        var text = innerNode.InnerText;
-                        if (!string.IsNullOrEmpty(text))
-                            ex.AppendLine(text.Trim());
-                    }
-                }
+                response = JObject.Parse(streamReader.ReadToEnd());    
             }
-            var explanation = ex.ToString();
-						
-            var imageUrl = "https://apod.nasa.gov/apod/" + relImageUrl;
-		
-            var indexExplanation = explanation.IndexOf("Explanation", StringComparison.Ordinal);
-            explanation = explanation.Substring(indexExplanation, explanation.Length - indexExplanation);
-		
-            var indexTomorrow = explanation.IndexOf("Tomorrow", StringComparison.Ordinal);
-            explanation = explanation.Substring(0, indexTomorrow);
+
+            var explanation = response["explanation"];	
+            var imageUrl = response["url"];
+            var title = response["title"];
             
             var embed = new EmbedBuilder();
-            embed.WithTitle("Astronomy Picture of the Day");
+            embed.WithTitle("APOD: " + title);
             embed.WithColor(new Color(0x5324b5));
-            embed.WithDescription(explanation);
-            embed.WithImageUrl(imageUrl);
+            embed.WithDescription(explanation.ToString());
+            embed.WithImageUrl(imageUrl.ToString());
             embed.WithCurrentTimestamp();
             embed.WithThumbnailUrl("https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png");
 
-            // await Context.Message.DeleteAsync();
+            await Context.Message.DeleteAsync();
             await Context.Channel.SendMessageAsync("", false, embed);
         }
     }
